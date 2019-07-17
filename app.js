0e935241752d21b82f12b8af30a3f565bedec92f
eduardojoya <ebjoya@gmail.com>
2019-07-16 20:32:44 -0700

    update post method so that clicking Read More renders correct blog post based on post_id

--- app.js
+++ app.js
@@ -70,17 +70,16 @@ app.post("/compose", function (req, res) {
   });
 });
 
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+  Post.findOne({
+    _id: requestedPostId
+  }, function (err, post) {
 
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
 });
 
